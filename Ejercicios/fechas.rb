dd992cf61c1d14cdb5cd39535932b39c81e5b3cf
Sofía Castillo <sofiaprogramadora@gmail.com>
2017-08-10 14:48:28 -0400

    Added more exercises

---
+++ Ejercicios/fechas.rb
@@ -0,0 +1,10 @@
+require 'date'
+
+# Crear el objeto Empresa y almacenar los pagos como arreglo
+# - Crear un método que muestre todos los pagos antes de una
+# fecha
+# - Crear un método que muestre todos los pagos registrados
+# posteriores al del día de hoy
+
+class Empresa
+end
