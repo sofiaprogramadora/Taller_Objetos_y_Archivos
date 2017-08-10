3f4cb9e97726ce92bb2bf696aa784499dc305125
Sofía Castillo <sofiaprogramadora@gmail.com>
2017-08-10 15:03:34 -0400

    Another change

--- Ejercicios/fechas.rb
+++ Ejercicios/fechas.rb
@@ -7,4 +7,22 @@ require 'date'
 # posteriores al del día de hoy
 
 class Empresa
+	def initialize(name, pagos = [])
+		@name = name
+		@pagos = pagos
+	end
 end
+
+file = File.open("fechas.txt", 'r')
+	lines = file.readlines
+file.close
+
+pago_empresas = []
+lines.each do |line|
+	data = line.chomp.split(" ")
+	fechas = data.dup
+	name = fechas.shift
+	pago_empresas << Empresa.new(name, fechas)
+	fechas.map{ |f| f.to_date }
+end
+print pago_empresas
